afec0d78e6984607d5dabec4c6797c0f47b02c9c
Pitsimash <Pitsimashitisho@gmail.com>
2023-06-15 09:50:20 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * Description: main - Prints the phrase "programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
